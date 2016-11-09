5a193fc00e2d03225a444cfe8e6ad77793df3716
Robert Arnesson <robert@robertarnesson.com>
2016-11-09 14:33:26 -0500

    geolocation

---
+++ plugin/geolocation.js
@@ -0,0 +1,67 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// cordova-plugin-geolocation
+var core_1 = require('@angular/core');
+require('rxjs/add/operator/mergeMap');
+var _1 = require('../');
+var PositionError = (function () {
+    function PositionError() {
+    }
+    Object.defineProperty(PositionError, "PERMISSION_DENIED", {
+        get: function () { return 1; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(PositionError, "POSITION_UNAVAILABLE", {
+        get: function () { return 2; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(PositionError, "TIMEOUT", {
+        get: function () { return 3; },
+        enumerable: true,
+        configurable: true
+    });
+    return PositionError;
+}());
+exports.PositionError = PositionError;
+var CameraService = (function () {
+    function CameraService(zone) {
+        this.zone = zone;
+    }
+    CameraService.prototype.getCurrentPosition = function () {
+        var _this = this;
+        return _1.Cordova.deviceready.mergeMap(function () { return _1.ZoneObservable.create(_this.zone, function (observer) {
+            window.navigator.geolocation.getCurrentPosition(function (res) {
+                observer.next(res);
+                observer.complete();
+            }, observer.error);
+        }); });
+    };
+    CameraService.prototype.watchPosition = function (options) {
+        var _this = this;
+        return _1.Cordova.deviceready.mergeMap(function () { return _1.ZoneObservable.create(_this.zone, function (observer) {
+            var watchID = window.navigator.geolocation.watchPosition(function (res) {
+                observer.next(res);
+                observer.complete();
+            }, observer.error, options);
+            return function () {
+                window.navigator.geolocation.clearWatch(watchID);
+            };
+        }); });
+    };
+    CameraService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [core_1.NgZone])
+    ], CameraService);
+    return CameraService;
+}());
+exports.CameraService = CameraService;
---
+++ plugin/geolocation.ts
@@ -0,0 +1,38 @@
+// cordova-plugin-geolocation
+import { Injectable, NgZone } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/mergeMap';
+
+import { Cordova, ZoneObservable } from '../';
+
+export class PositionError {
+    public static get PERMISSION_DENIED(): number { return 1; }
+    public static get POSITION_UNAVAILABLE(): number { return 2; }
+    public static get TIMEOUT(): number { return 3; }
+}
+
+@Injectable()
+export class CameraService {
+    constructor(private zone: NgZone) {}
+
+    getCurrentPosition(): Observable<any> {
+        return Cordova.deviceready.mergeMap(() => ZoneObservable.create(this.zone, (observer: any) => {
+            (<any>window).navigator.geolocation.getCurrentPosition((res: any) => {
+                observer.next(res);
+                observer.complete();
+            }, observer.error);
+        }));
+    }
+    watchPosition(options?: Object): Observable<any> {
+        return Cordova.deviceready.mergeMap(() => ZoneObservable.create(this.zone, (observer: any) => {
+            let watchID = (<any>window).navigator.geolocation.watchPosition((res: any) => {
+                observer.next(res);
+                observer.complete();
+            }, observer.error, options);
+            return () => {
+                (<any>window).navigator.geolocation.clearWatch(watchID);
+            };
+        }));
+    }
+}
